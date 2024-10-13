ab0c2d1aeb056c153ae159b292566a016104036f
naman_kumar_mahto <mahtokumarnaman@gmail.com>
2024-10-13 21:29:51 +0530

    2.0 update pop up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.neettimer"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
